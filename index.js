21a455795b558daf46abd4d987d36181e79a7b25
rokity <riccardo.amadio1@gmail.com>
2018-12-13 23:08:47 +0100

    add comment

--- index.js
+++ index.js
@@ -1,21 +1,22 @@
 const fs = require('fs');
 const path = require('path');
 const clear_file = require('./clear_file.js');
-
-var name_file = "file4.txt";
+//take input of the file
+const name_file = process.argv[2];
 const filePath = path.join(__dirname, name_file);
-
+//read the file
 let data = fs.readFileSync(filePath, { encoding: 'utf-8' });
+//clear the begin and the end of the file from signed file
 data = clear_file.clear_begin(data);
 data = clear_file.clean_end(data);
-
-for(let i=0;i<data.length;i++)
-{
-    if(data[i]!= undefined && !(data[i].charCodeAt(0)<127))
-        {
-            let prima = data.substr(0,i-1);
-            let dopo = data.substr(i+3,data.length-i);
-            data = prima+dopo;
-        }
+//Clear the file from special characters
+for (let i = 0; i < data.length; i++) {
+    if (data[i] != undefined && !(data[i].charCodeAt(0) < 127)) {
+        let prima = data.substr(0, i - 1);
+        let dopo = data.substr(i + 3, data.length - i);
+        data = prima + dopo;
+    }
 }
-fs.writeFileSync(path.join(__dirname, name_file+".finito"),data)
+//Write the file
+fs.writeFileSync(path.join(__dirname, name_file + ".finito"), data);
+console.log(data);
