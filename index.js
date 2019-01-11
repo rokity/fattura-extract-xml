538b5b5260771f84fc99e634807d7ce1344b2da5
Riccardo Amadio aka rokity <riccardo.amadio1@gmail.com>
2019-01-11 20:28:29 +0100

    refactoring and add input check, watch comment on clear_file.js

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
-const fs = require('fs');
-const path = require('path');
-const clear_file = require('./clear_file.js');
+const fs = require('fs')
+const path = require('path')
+const ClearFile = require('./clear_file.js')
+const write_file = require('./write_file.js')
 //take input of the file
-const name_file = process.argv[2];
-const filePath = path.join(__dirname, name_file);
+const name_file = process.argv[2]
+const filePath = path.join(__dirname, name_file)
 //read the file
-let data = fs.readFileSync(filePath, { encoding: 'utf-8' });
+let data = fs.readFileSync(filePath, { encoding: 'utf-8' })
 //clear the begin and the end of the file from signed file
-data = clear_file.clear_begin(data);
-data = clear_file.clean_end(data);
-//Clear the file from special characters
-for (let i = 0; i < data.length; i++) {
-    if (data[i] != undefined && !(data[i].charCodeAt(0) < 127)) {
-        let prima = data.substr(0, i - 1);
-        let dopo = data.substr(i + 3, data.length - i);
-        data = prima + dopo;
-    }
-}
+let clearFile= new ClearFile(name_file)
+data = clearFile.clear_begin(data)
+data = clearFile.clean_end(data)
+//clear from special characters 
+data = clearFile.clear_special_characters(data)
 //Write the file
-fs.writeFileSync(path.join(__dirname, name_file + ".finito"), data);
-console.log(data);
+write_file(name_file, data)
+console.log(data)
+
+
+
+
