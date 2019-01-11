39cc5b772797baa026059ccf92f86d2db8cc64e7
Riccardo Amadio aka rokity <riccardo.amadio1@gmail.com>
2019-01-11 20:28:08 +0100

    refactoring and create a class The class work as the last release but I add two check on the input. If the input it's not fimerd it will write the file in the output and exit from the program.

--- clear_file.js
+++ clear_file.js
@@ -1,12 +1,45 @@
+const write_file = require('./write_file.js')
 
-exports.clear_begin = (data)=>
-{
-    var begin_xml = data.indexOf("<?xml");
-    return data.substr(begin_xml,data.length-begin_xml);
+class ClearFile {
+    constructor(name_file) {
+        this.name_file = name_file
+    }
+
+    clear_begin(data) {
+        var begin_xml = data.indexOf("<?xml");
+        if(begin_xml!=0)
+            return data.substr(begin_xml, data.length - begin_xml);
+        else{
+            //it's not signed
+            write_file(this.name_file, data)
+            process.exit(1)
+        }
+        }
+
+    clean_end(data) {
+        var end_xml = data.indexOf("</p:FatturaElettronica>");
+        if ((end_xml + 23) < data.length)
+            return data.substr(0, end_xml + 23)
+        else {
+            //it's not signed
+            write_file(this.name_file, data)
+            process.exit(1)
+        }
+
+    }
+    clear_special_characters(data) {
+        //Clear the file from special characters
+        //char ascii (char<127)
+        for (let i = 0; i < data.length; i++) {
+            if (data[i] != undefined && !(data[i].charCodeAt(0) < 127)) {
+                let prima = data.substr(0, i - 1)
+                let dopo = data.substr(i + 3, data.length - i)
+                data = prima + dopo
+            }
+        }
+        return data
+    }
+
 }
 
-exports.clean_end = (data) =>
-{
-    var end_xml = data.indexOf("</p:FatturaElettronica>");
-    return data.substr(0,end_xml+23);
-}
+module.exports = ClearFile
